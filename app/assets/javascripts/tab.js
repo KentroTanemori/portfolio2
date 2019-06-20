117fe11dd2ccd5c74bec86dece5da8606eb9e3df
KentaroTanemori <0ja0t12t723120n@ezweb.ne.jp>
2019-06-20 06:13:36 +0000

    メッセージ

---
+++ app/assets/javascripts/tab.js
@@ -0,0 +1,8 @@
+// $(".tab_label").on("click",function(){
+// 	alert("fire!!!!!");
+// 	var $th = $(this).index();
+// 	$(".tab_label").removeClass("active");
+// 	$(".tab_panel").removeClass("active");
+// 	$(this).addClass("active");
+// 	$(".tab_panel").eq($th).addClass("active");
+// });
